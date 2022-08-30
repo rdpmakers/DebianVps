38591c05be4dfbc172b07bf65df2175dc24e7330
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-08-30 11:25:22 +0700

    Update CRD.sh

--- CRD.sh
+++ CRD.sh
@@ -5,11 +5,6 @@ echo "============================="
 sudo apt-get update
 clear
 echo "============================="
-echo "   updgrading, please wait"
-echo "============================="
-sudo apt-get upgrade -y
-clear
-echo "============================="
 echo "      installing lxde"
 echo "============================="
 sudo apt-get install lxde -y
@@ -18,7 +13,7 @@ echo "============================="
 echo "      installing CRD"
 echo "============================="
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-./chrome-remote-desktop_current_amd64.deb
+sudo ./chrome-remote-desktop_current_amd64.deb
 echo "========================================================="
 echo " get crp from: https://remotedesktop.google.com/headless"
 echo "      paste the crp on command then setup your rdp"
