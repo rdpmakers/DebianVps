65e1f84ae534b08ef6ba50754835c40a712b2874
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-08-30 19:36:15 +0700

    Update PlayOnLinux.sh

--- PlayOnLinux.sh
+++ PlayOnLinux.sh
@@ -1,1 +1,41 @@
-
+clear
+clear
+echo "============================="
+echo "     adding architecture"
+echo "============================="
+sudo dpkg --add-architecture i386
+clear
+clear
+echo "============================="
+echo "          updating"
+echo "============================="
+sudo apt-get update
+clear
+clear
+echo "============================="
+echo "      installing wine32"
+echo "============================="
+sudo apt-get install wine32 -y
+clear
+clear
+echo "============================="
+echo "   Downloading PlayOnLinux"
+echo "============================="
+cd ~/Downloads
+wget https://www.playonlinux.com/script_files/PlayOnLinux/4.3.4/PlayOnLinux_4.3.4.deb
+clear
+clear
+echo "============================="
+echo "     getting version info"
+echo "============================="
+playonlinux --version
+clear
+clear
+echo "============================="
+echo "     starting PlayOnLinux"
+echo "============================="
+playonlinux
+echo "============================="
+echo "          Task Done"
+echo "============================="
+cd
