a7651f54976c6ffb1f7f60f02fb52e09b098c041
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-08-30 11:13:15 +0700

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -30,13 +30,6 @@ echo "============================="
 sudo adduser desktop --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "desktop:Debian" | sudo chpasswd
 sudo usermod -aG sudo,adm desktop
-echo " ________      _________        ________"
-echo "| _______ \   | ________ \     |  ______ \"
-echo "| |      | |  | |       | |    | |      | |"
-echo "| |_____/  /  | |       | |    | |______| |"
-echo "|  _____  /   | |       | |    |  _______/"
-echo "| |     \ \   | |_______| |    | |"
-echo "|_|      \_\  |__________/     |_|"
 echo "====================="
 echo "  username: desktop"
 echo "  password: Debian"
