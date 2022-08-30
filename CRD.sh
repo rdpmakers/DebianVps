701e77881de8aa3d7af86263f674c7a803f3a6db
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-08-30 11:37:37 +0700

    Update CRD.sh

--- CRD.sh
+++ CRD.sh
@@ -14,7 +14,15 @@ echo "      installing CRD"
 echo "============================="
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install ./chrome-remote-desktop_current_amd64.deb
+echo "============================="
+echo "         adding user"
+echo "============================="
+sudo adduser desktop --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "desktop:desktop" | sudo chpasswd
+sudo usermod -aG sudo,adm desktop
+clear
 echo "========================================================="
 echo " get crp from: https://remotedesktop.google.com/headless"
 echo "      paste the crp on command then setup your rdp"
 echo "========================================================="
+echo " note: use this password if you are using a 'sudo' command : desktop"
