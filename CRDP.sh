62ea65e80651765f45c94a1bcb4d9ccd95e93561
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-08 14:17:21 +0700

    Update CRDP.sh

--- CRDP.sh
+++ CRDP.sh
@@ -25,7 +25,7 @@ echo "  installing chrome browser"
 echo "============================="
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install -f
+sudo apt-get -f install -y
 clear
 clear
 echo "============================="
