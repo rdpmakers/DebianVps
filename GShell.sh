e870e633f52ab87940450bed3dafae29ce7d07b4
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-23 20:54:50 +0700

    Update GShell.sh

--- GShell.sh
+++ GShell.sh
@@ -25,6 +25,8 @@ echo "  installing chrome browser"
 echo "============================="
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+clear
+clear
 echo "============================="
 echo "    setting up the XRDP"
 echo "============================="
