c427e58a14f3ea2292223d72a940513b4265c258
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-10-08 10:41:17 +0700

    Update SysVinitX.sh

--- SysVinitX.sh
+++ SysVinitX.sh
@@ -20,6 +20,25 @@ install -t "/var/lib/xrdp-pulseaudio-installer" -D -m 644 *.so
 # systemctl restart pulseaudio
 service xrdp restart
 
+mkdir -p /home/desktop/.config/systemd/user/
+ln -s /dev/null /home/desktop/.config/systemd/user/pulseaudio.service
+mkdir -p /home/desktop/.config/autostart/
+cat <<EOF | \
+  sudo tee /home/desktop/.config/autostart/pulseaudio.desktop
+[Desktop Entry]
+Type=Application
+Exec=pulseaudio
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=pulseaudio
+Name=pulseaudio
+Comment[en_US]=pulseaudio
+Comment=pulseaudio
+EOF
+chown -R desktop /home/desktop/.config/
+chmod -R 755 /home/desktop/.config/
+
 useradd -s /bin/bash -m desktop
 usermod -a -G sudo desktop
 echo "desktop ALL=(ALL) ALL" >> /etc/sudoers
