1b3a107fed506524eec856a41fe17fa5b94c07b1
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-10-09 08:12:15 +0700

    Create S.sh

---
+++ S.sh
@@ -0,0 +1,21 @@
+apt-get install -y git libpulse-dev autoconf m4 intltool build-essential dpkg-dev libtool libsndfile1-dev libspeexdsp-dev libudev-dev pulseaudio
+cp /etc/apt/sources.list /etc/apt/sources.list.u2ad
+sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+apt-get update -y
+apt build-dep pulseaudio -y
+cd /tmp
+apt source pulseaudio
+pulsever=$(pulseaudio --version | awk '{print $2}')
+cd /tmp/pulseaudio-$pulsever
+# ./configure --without-caps
+./configure
+git clone https://github.com/neutrinolabs/pulseaudio-module-xrdp.git
+cd pulseaudio-module-xrdp
+./bootstrap
+./configure PULSE_DIR="/tmp/pulseaudio-$pulsever"
+make
+cd /tmp/pulseaudio-$pulsever/pulseaudio-module-xrdp/src/.libs
+install -t "/var/lib/xrdp-pulseaudio-installer" -D -m 644 *.so
+# systemctl restart dbus
+# systemctl restart pulseaudio
+service xrdp restart
