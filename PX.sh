93d0da6ecb1d91baab2c8a5aacc66b98f3cb83d2
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2023-02-03 05:17:56 +0700

    Create PX.sh

---
+++ PX.sh
@@ -0,0 +1,5 @@
+sudo apt install software-properties-common
+wget https://c-nergy.be/downloads/xRDP/xrdp-installer-1.4.6.zip
+unzip xrdp-installer-1.4.6.zip
+chmod +x xrdp-installer-1.4.6.sh
+./*.6.sh -s
