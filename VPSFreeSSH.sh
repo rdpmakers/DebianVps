03ed0fbe9edd522f2aea67ed6044ff1e10dd2de3
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2023-03-01 20:00:53 +0700

    Update and rename VPSFreeSSH to VPSFreeSSH.sh

--- VPSFreeSSH.sh
+++ VPSFreeSSH.sh
@@ -12,6 +12,6 @@ wget https://raw.githubusercontent.com/rdpmakers/MyRDP/main/NSSH.service
 mv NSSH.service /etc/systemd/system/
 service NSSH start
 echo "============================"
-echo "ssh domain:" && curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Login with your user and password"
+echo "ssh domain:" && curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "============================"
