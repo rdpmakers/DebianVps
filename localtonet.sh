124955e61a474218776193eb8ad0a54d7340c498
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2023-03-22 19:48:20 +0700

    Update localtonet.sh

--- localtonet.sh
+++ localtonet.sh
@@ -1,3 +1,9 @@
 wget https://raw.githubusercontent.com/rdpmakers/DebianVps/main/localtonet.service
 mv localtonet.service /etc/systemd/system
-wget -O localtonet.zip 
+wget -O localtonet.zip https://localtonet.com/download/localtonet-linux-x64.zip
+apt install unzip -y
+unzip localtonet.zip
+mv localtonet /usr/local/bin
+systemctl enable localtonet
+systemctl start localtonet
+screen -r localtonet
